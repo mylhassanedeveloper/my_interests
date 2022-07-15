70df3cef53f833f68114abf4bc7fc56021815aed
My Lhassane <mastinoz_7@hotmail.com>
2022-07-15 17:24:50 +0100

    removed use lib from i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,4 @@
-import i18n, { use } from "i18next";
+import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import Languagedetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
