b9a77e4da223bcbfb65dc95966abc96090ff3d69
My Lhassane <mastinoz_7@hotmail.com>
2022-07-15 17:53:14 +0100

    Updates

---
+++ precache-manifest.af58c724eb1b940eef2cf6e35c7438fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0dc7be52cfb824f9c01",
+    "url": "/my-interests/static/css/main.44929471.chunk.css"
+  },
+  {
+    "revision": "d0dc7be52cfb824f9c01",
+    "url": "/my-interests/static/js/main.d0dc7be5.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/my-interests/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "2842b9e81751befa72b5",
+    "url": "/my-interests/static/js/2.2842b9e8.chunk.js"
+  },
+  {
+    "revision": "56e3595680bcec942cd2",
+    "url": "/my-interests/static/js/runtime~main.56e35956.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/my-interests/static/media/getFetch.51c934af.cjs"
+  },
+  {
+    "revision": "00830e360eb4b64ae4aa4ce8c4768da9",
+    "url": "/my-interests/index.html"
+  }
+];
